e8d6af7ef1317792234d33d8457f268df52c8cfc
Adnan Kamili <adnan.kamili@gmail.com>
2019-02-23 17:47:38 +0530

    updated upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -3,9 +3,6 @@
 echo "### stopping the Cryptlex services ..."
 docker-compose stop
 
-echo "### getting the latest changes ..."
-git pull
-
 echo "### getting the latest webapi and dashboard docker images ..."
 docker-compose pull webapi dashboard
 
