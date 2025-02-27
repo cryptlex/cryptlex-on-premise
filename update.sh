a483c84b9da1df36801e50d86fb378217d8b9360
Adnan Kamili <adnan-kamili@users.noreply.github.com>
2025-02-27 16:00:19 +0530

    chore: Update update.sh

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "### getting the latest docker images ..."
-docker compose pull web-api dashboard admin-portal customer-portal reseller-portal release-server
+docker compose pull web-api dashboard admin-portal customer-portal reseller-portal release-server geoip
 
 echo "### stopping the services ..."
-docker compose stop web-api dashboard admin-portal customer-portal reseller-portal release-server
+docker compose stop web-api dashboard admin-portal customer-portal reseller-portal release-server geoip
 
 echo "### restarting the services ..."
 docker compose up -d
